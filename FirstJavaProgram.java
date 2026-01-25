76eb8cee666f7720df56338b7ab3c1896daa300a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-25 16:30:02 +0600

    1st java

---
+++ FirstJavaProgram.java
@@ -0,0 +1,39 @@
+package firstjavaprogram;
+
+import java.util.Scanner;
+
+public class FirstJavaProgram {
+
+    public static void main(String[] args) {
+//        Scanner input =new Scanner(System.in);
+//        
+//        System.out.println("Please Enter Radius ");
+//        
+//        float radius = input.nextFloat();
+//        
+//        float area;
+//        
+//        double perimeter;
+//        
+//        
+//        System.out.println("Radius is : "+ radius);
+//        
+//       perimeter =  2*radius*3.14159;
+////        
+//        System.out.println("Perimeter of the circle is : " + perimeter);
+//
+//        area = (float) 3.14159 * radius * radius;
+//        System.out.println("Area of the circle is : " + area);
+        
+        System.out.println("What is your name?");
+        Scanner output =new Scanner(System.in);
+        
+        
+        String names = output.nextLine();
+        
+        System.out.println("My name is " + names);
+                
+                
+    }
+
+}
