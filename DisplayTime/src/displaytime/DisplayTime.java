3e73b1ba59524b85e84d8a3460e8f82f9e097c26
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-25 18:11:49 +0600

    dgf

--- DisplayTime/src/displaytime/DisplayTime.java
+++ DisplayTime/src/displaytime/DisplayTime.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package displaytime;
 
-/**
- *
- * @author Admin
- */
+import java.util.Scanner;
+
+
 public class DisplayTime {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Enter Second : ");
+        Scanner input = new Scanner(System.in);
+        
+        int seconds = input.nextInt();
+        
+        int minutes = seconds/60;
+        
+        int second = seconds % 60;
+        
+        System.out.println(minutes +" Minutes "+ second +" Seconds ");
     }
     
 }
