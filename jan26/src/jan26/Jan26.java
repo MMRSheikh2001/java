f46e48a9ab1d0d03db4f55a862faa4f672460bbc
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-26 18:34:11 +0600

    gf

--- jan26/src/jan26/Jan26.java
+++ jan26/src/jan26/Jan26.java
@@ -1,23 +1,26 @@
-
 package jan26;
 
 import java.util.Scanner;
 
-
 public class Jan26 {
 
-  
     public static void main(String[] args) {
-        System.out.println("Give 1st Number ");
-        Scanner outputa = new Scanner(System.in);
-        float numbera = outputa.nextFloat();
-        
-        System.out.println("Give 2nd Number ");
-        Scanner outputb = new Scanner(System.in);
-        float numberb = outputb.nextFloat();
-        
-        double numberc = numbera + numberb;
-        System.out.println("The sum is : " + numberc);
+//        System.out.println("Give 1st Number ");
+//        Scanner outputa = new Scanner(System.in);
+//        float numbera = outputa.nextFloat();
+//        
+//        System.out.println("Give 2nd Number ");
+//    
+//        float numberb = outputa.nextFloat();
+//        
+//        double numberc = numbera + numberb;
+//        System.out.println("The sum is : " + numberc);
+
+        System.out.println("What is your Name?");
+        Scanner input = new Scanner(System.in);
+        String myName = input.nextLine();
+        System.out.println("Your Name is "+ myName);
+
     }
-    
+
 }
