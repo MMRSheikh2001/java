0cc2f13f3668d379f703fafb7952b9fb540b2d6e
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-27 15:59:16 +0600

    fggf

--- Jan27/src/jan27/Jan27.java
+++ Jan27/src/jan27/Jan27.java
@@ -9,15 +9,31 @@ public class Jan27 {
    
     public static void main(String[] args) {
         
-        int i = 10;
-        i++;
-        i+=5;
-        Scanner input = new Scanner(System.in);
-        System.out.println(++i);
+//        int i = 10;
+//        i++;
+//        i*=5;
+//       Scanner input = new Scanner(System.in);
+//        System.out.println(++i);
+//        
+//        int j = input.nextInt();
+//        
+//        System.out.println(i + j);
+
+  //   int year = input.nextInt();
+
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Give Radius");
         
-        int j = input.nextInt();
+        float radius = scanner.nextFloat();
+        float areaOfCircle;
+        if(radius < 0){
+            System.out.println("Invalid Radius");
+        } else{
+            areaOfCircle = (float)(Math.PI * Math.pow(radius,2 ));
+            System.out.println("The area of Circle is : "+ areaOfCircle);
+        }
         
-        System.out.println(i + j);
+        
     }
     
 }
