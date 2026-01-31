6a87365aa0fc7ad4324c6faea67bd2457811f38d
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-31 17:15:36 +0600

    sd

---
+++ Jan31/src/jan31/primeNumber.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jan31;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class primeNumber {
+    
+    public static void main(String[] args) {
+        System.out.println("Give the Number");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int count = 0;
+        for(int i = 1;i<= num;i++){
+            if(num % i == 0){
+            count =count + 1;
+            }
+        }
+        
+        if(count ==2){
+            System.out.println("It is Prime Number");
+            
+        } else{
+        System.out.println("It is Not Prime Number");
+        }
+    }
+    
+}
