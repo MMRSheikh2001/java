1d7e51cd7d64b38fe22ce19905257203f0d9aaf9
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-01 16:32:10 +0600

    dgf

--- Feb1/src/feb1/Feb1.java
+++ Feb1/src/feb1/Feb1.java
@@ -1,14 +1,23 @@
-
 package feb1;
 
-
+import java.util.Scanner;
 
 public class Feb1 {
 
-   
-    
     public static void main(String[] args) {
-        
+        System.out.println("Give the number whose factorial you want");
+        Scanner s = new Scanner(System.in);
+        int userInput = s.nextInt();
+        int output = factorial(userInput);
+        System.out.println("The factorial of " + userInput + " is " + output);
     }
-    
+
+    public static int factorial(int number) {
+        int m = 1;
+        for (int i = 1; i <= number; i++) {
+            m = i * m;
+        }
+        return m;
+    }
+
 }
