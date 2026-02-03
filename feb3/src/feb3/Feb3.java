a5eadef9e9b70e343d116f46f9c31aafa9f1803c
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-03 17:09:47 +0600

    tttttttt

--- feb3/src/feb3/Feb3.java
+++ feb3/src/feb3/Feb3.java
@@ -8,25 +8,25 @@ import java.util.Scanner;
  * @author Admin
  */
 public class Feb3 {
-    
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("How many numbers do you want to Sort?");
         int n = s.nextInt();
-        
+
         int numbers[] = new int[n];
         System.out.println("Beginning of Loop \n" + Arrays.toString(numbers));
-        
+
         for (int i = 0; i < n; i++) {
             System.out.println("Give the " + (i + 1) + " th Number");
             int userInput = s.nextInt();
             numbers[i] = userInput;
         }
         System.out.println("End of Loop \n" + Arrays.toString(numbers));
-         numbers = bubleSort(numbers);
+        numbers = bubleSort(numbers);
         System.out.println("The Sorted array is " + Arrays.toString(numbers));
     }
-    
+
     public static int[] bubleSort(int array[]) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
@@ -39,5 +39,5 @@ public class Feb3 {
         }
         return array;
     }
-    
+
 }
