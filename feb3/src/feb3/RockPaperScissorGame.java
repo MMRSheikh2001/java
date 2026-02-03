df9b739f406d13417f97ab4749c7fc87a9982c3a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-03 17:46:22 +0600

    abcdefghijklmnopqrstuvwxyz

--- feb3/src/feb3/RockPaperScissorGame.java
+++ feb3/src/feb3/RockPaperScissorGame.java
@@ -9,8 +9,19 @@ package feb3;
  * @author Admin
  */
 public class RockPaperScissorGame {
+
     public static void main(String[] args) {
-        
+        int[] numbers = {3, -1, 7, 0, 9};
+
+        for (int n : numbers) {
+            if (n < 0) {
+                continue; // skip negative numbers
+            }
+            if (n == 0) {
+                break; // stop loop when zero is found
+            }
+            System.out.println(n);
+        }
     }
-    
+
 }
