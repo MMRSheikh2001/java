322646877c5b6cdb12ca095dfd22d35d3352e730
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-03 18:34:56 +0600

    rrty

--- feb3/src/feb3/RockPaperScissorGame.java
+++ feb3/src/feb3/RockPaperScissorGame.java
@@ -16,7 +16,11 @@ public class RockPaperScissorGame {
         System.out.println(random);
 
         if (random == 0 || random == 3 || random == 6 || random == 9) {
-            random = 0;
+            random = 1;
+        } else if (random == 1 || random == 4 || random == 7) {
+            random = 2;
+        } else {
+            random = 3;
         }
     }
 
---
+++ feb3/src/feb3/binarySorting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package feb3;
+
+/**
+ *
+ * @author Admin
+ */
+public class binarySorting {
+    public static void main(String[] args) {
+        
+    }
+    
+}
