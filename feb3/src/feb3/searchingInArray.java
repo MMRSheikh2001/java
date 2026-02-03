4f4a7fb891eb30aeba8b10970948ce00fcedcd80
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-03 17:39:51 +0600

    abcdefghijklmnopqrstuvwxyz

--- feb3/src/feb3/searchingInArray.java
+++ feb3/src/feb3/searchingInArray.java
@@ -7,7 +7,21 @@ package feb3;
 public class searchingInArray {
 
     public static void main(String[] args) {
+        int numbers[]={12,34,34,232,23,43,54};
+        int search = 232;
+        int indexNo= linearSearch(numbers, search);
+        System.out.println(indexNo);
         
+    }
+    public static int linearSearch(int[] array,int searchNumber){
+        
+        for(int index =0;index<array.length;index++){
+        if(array[index]== searchNumber){
+        return index;
+        
+        }
+        }
+    return -1;
     }
 
 }
