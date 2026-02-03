331976311d65746661620e9da8a3e6aa1a173389
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-03 16:08:03 +0600

    fh

--- feb3/src/feb3/Feb3.java
+++ feb3/src/feb3/Feb3.java
@@ -1,5 +1,7 @@
-
 package feb3;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -7,9 +9,35 @@ package feb3;
  */
 public class Feb3 {
 
-    
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many numbers do you want to Sort?");
+        int n = s.nextInt();
+
+        int numbers[] = new int[n];
+        System.out.println("Beginning of Loop \n" + Arrays.toString(numbers));
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Give the " + (i + 1) + " th Number");
+            int userInput = s.nextInt();
+            numbers[i] = userInput;
+        }
+        System.out.println("End of Loop \n" + Arrays.toString(numbers));
+        int newArray[]= bubleSort(numbers);
+        System.out.println("The Sorted array is "+ Arrays.toString(newArray));
     }
-    
+
+    public static int[] bubleSort(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
 }
