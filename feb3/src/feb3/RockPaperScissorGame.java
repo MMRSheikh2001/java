1a11c88c991a94adfa803a49f51b770ce9223afe
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-03 19:00:00 +0600

    gfh

--- feb3/src/feb3/RockPaperScissorGame.java
+++ feb3/src/feb3/RockPaperScissorGame.java
@@ -4,6 +4,8 @@
  */
 package feb3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -12,16 +14,33 @@ public class RockPaperScissorGame {
 
     public static void main(String[] args) {
         int random = (int) (10 * Math.random());
+        Scanner s = new Scanner(System.in);
+        System.out.println("Give the number representing Rock or Paper or Scissor");
+        System.out.println("1 = Rock \n2 = Paper \n3 = Scissor");
+        int userInput = s.nextInt();
 
-        System.out.println(random);
-
+        //11       System.out.println(random);
         if (random == 0 || random == 3 || random == 6 || random == 9) {
             random = 1;
+            System.out.println("System says \"Rock\"");
         } else if (random == 1 || random == 4 || random == 7) {
             random = 2;
+            System.out.println("System says \"Paper\"");
         } else {
             random = 3;
+            System.out.println("System says \"Scissor\"");
         }
+        if(userInput == random){
+            System.out.println("It's a draw");
+        }
+        else if((userInput ==1 && random ==3) || (userInput ==2 && random ==1)||(userInput ==3 && random ==2)){
+            System.out.println("You Win");
+        }
+        else{
+        System.out.println("You Lose");
+                
+        }
+            
     }
 
 }
