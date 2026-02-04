417773cf350b8e4557b7f603e1367beb162d7ff4
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-04 13:44:38 +0600

    dgdgdg

--- feb3/src/feb3/binarySearching.java
+++ feb3/src/feb3/binarySearching.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author Admin
  */
 public class binarySearching {
-
+    
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Give the array length");
@@ -24,22 +24,50 @@ public class binarySearching {
             numbers[i] = s.nextInt();
         }
         System.out.println("After the Loop " + Arrays.toString(numbers));
+        System.out.println("Give the number whose index position you want in array");
+        int userSearchingNumber = s.nextInt();
+        linearSearch(numbers, userSearchingNumber);
+        
         sort(numbers);
-
+        
     }
-
+    
     public static void sort(int array[]) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
                 //ascending order sorting
-                if(array[j] > array[j+1]){
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
-        System.out.println("The ascending order sorted array is "+ Arrays.toString(array));
+        System.out.println("The ascending order sorted array is " + Arrays.toString(array));
     }
-
+    
+    public static int binarySearch(int array[], int searchNumber) {
+        int firstIndex = 0;
+        int lastIndex = array.length - 1;
+        int midIndex = (int) (firstIndex + lastIndex) / 2;
+        int indexPosition = 0;
+        for (int i = 0; i <= midIndex; i++) {
+        }
+        return 0;
+    }
+    
+    public static void linearSearch(int array[], int searchNumber) {
+        boolean found = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == searchNumber) {
+                System.out.println("The Number has been found in index position " + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("The Number is not found in Array");
+        }
+    }
+    
 }
