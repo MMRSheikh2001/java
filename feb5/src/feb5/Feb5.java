6de2b9bf4f04fd4bbf669e59ce8f0f33aa8718c0
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-05 15:30:18 +0600

    dgd

--- feb5/src/feb5/Feb5.java
+++ feb5/src/feb5/Feb5.java
@@ -21,8 +21,13 @@ public class Feb5 {
             numbers[i] = s.nextInt();
         }
         System.out.println("After the Loop " + Arrays.toString(numbers));
+        
+        Arrays.sort(numbers);
+        System.out.println("The sorted array is "+ Arrays.toString(numbers));
+        
         System.out.println("Give the number whose index position you want in array");
         int userSearchingNumber = s.nextInt();
+        System.out.println("The linear search position is " + Arrays.binarySearch(numbers, userSearchingNumber));
     }
 
 }
