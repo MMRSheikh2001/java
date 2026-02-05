c43d20b42b30d8ef0974d56644516035c755ad02
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-05 15:40:57 +0600

    fggf

---
+++ feb5/src/feb5/multiDimensionalArray.java
@@ -0,0 +1,16 @@
+package feb5;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Admin
+ */
+public class multiDimensionalArray {
+
+    public static void main(String[] args) {
+        int numbers[][]=new int[3][4];
+        
+        System.out.println(Arrays.deepToString(numbers));
+    }
+}
