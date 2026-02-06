ced2a3b1ae8d8fb946d617726128819e964c1572
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-07 03:51:35 +0600

    dd

--- feb5/src/feb5/matrixSum.java
+++ feb5/src/feb5/matrixSum.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author Mahbub
  */
 public class matrixSum {
-    
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Give row number for 1st matrix");
@@ -23,23 +23,25 @@ public class matrixSum {
         int rows2 = s.nextInt();
         System.out.println("Give column number for 2nd matrix");
         int cols2 = s.nextInt();
-        
+
         int[][] matrix1 = new int[rows1][cols1];
         int[][] matrix2 = new int[rows2][cols2];
-        
+
         inputMatrix(rows1, cols1, matrix1, "Matrix 1");
+        matrixFormat(matrix1);
         inputMatrix(rows2, cols2, matrix2, "Matrix 2");
-        
+        matrixFormat(matrix2);
         int[][] sumMatrix = new int[rows2][cols1];
         for (int i = 0; i < rows2; i++) {
             for (int j = 0; j < cols1; j++) {
                 sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
-        System.out.println(Arrays.deepToString(sumMatrix));
-        
+        System.out.println("The Sum of 2 matrices is " + Arrays.deepToString(sumMatrix));
+        matrixFormat(sumMatrix);
+
     }
-    
+
     public static void inputMatrix(int row, int col, int[][] array, String name) {
         System.out.println("Give values for the " + name);
         Scanner sc = new Scanner(System.in);
@@ -51,5 +53,15 @@ public class matrixSum {
         }
         System.out.println("The array is \n" + Arrays.deepToString(array));
     }
-    
+
+    public static void matrixFormat(int[][] array) {
+        System.out.println("The matrix Format array is ");
+        for (int row[] : array) {
+            for (int single : row) {
+                System.out.print(single + "\t");
+            }
+            System.out.println("\n");
+        }
+    }
+
 }
