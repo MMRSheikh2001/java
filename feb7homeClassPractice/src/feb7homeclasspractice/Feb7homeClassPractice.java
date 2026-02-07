950e31424691b181c96b22bf8ff1608fbcb4f07a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-07 21:19:56 +0600

    dd

---
+++ feb7homeClassPractice/src/feb7homeclasspractice/Feb7homeClassPractice.java
@@ -0,0 +1,15 @@
+
+package feb7homeclasspractice;
+
+/**
+ *
+ * @author Mahbub
+ */
+public class Feb7homeClassPractice {
+
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
