e17de6677f0a417853d45197c7784aac6ef49887
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-08 23:49:57 +0600

    ff

--- feb8/src/feb8/pos/CalculatorOperations.java
+++ feb8/src/feb8/pos/CalculatorOperations.java
@@ -5,5 +5,63 @@ package feb8.pos;
  * @author Mahbub
  */
 public class CalculatorOperations {
+
+    private double length, width, radius;
+    private float height;
+
+    public CalculatorOperations() {
+    }
+
+    public CalculatorOperations(double radius) {
+        this.radius = radius;
+    }
+
+    public CalculatorOperations(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public CalculatorOperations(double length, float height) {
+        this.length = length;
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+    
+    
+    
+    
+    
+    
     
 }
