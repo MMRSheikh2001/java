c0dac6f7d808c9ad75ccf7074a4d3e0b52616847
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-12 18:30:37 +0600

    fsfd

--- feb11HomePractice/src/feb11homepractice/Feb11HomePractice.java
+++ feb11HomePractice/src/feb11homepractice/Feb11HomePractice.java
@@ -1,6 +1,8 @@
 
 package feb11homepractice;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mahbub
@@ -10,6 +12,21 @@ public class Feb11HomePractice {
    
     public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            for(int j=1; j<=n;j++){
+                int s =(int) ((int) a + Math.pow(2, j-1) *b);
+                System.out.println(s);
+            }
+            
+            
+        }
+        in.close();
     }
     
 }
