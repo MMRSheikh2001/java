14a4c6e84f8eb0549aabcc69c067ca333bdceae1
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-14 18:02:44 +0600

    rtyty

--- feb14/src/feb14/Feb14.java
+++ feb14/src/feb14/Feb14.java
@@ -17,8 +17,17 @@ public class Feb14 {
         try {
             File file = new File("C:\\Users\\Admin\\Desktop\\javaPracticeFile.txt");
             pw = new PrintWriter(file);
-            pw.write("Hello Sabbir \n");
-            pw.println("From  Mahbub");
+//            pw.write("Hello Sabbir \n");
+//            pw.println("From  Mahbub");
+
+            for (int i = 1; i <= 100; i++) {
+
+                pw.print(i + "   ");
+                if (i % 10 == 0) {
+                    pw.print("\n");
+                }
+
+            }
             pw.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Feb14.class.getName()).log(Level.SEVERE, null, ex);
