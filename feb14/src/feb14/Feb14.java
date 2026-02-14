db99860c2febf2a69ccf2026c65feb782706a726
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-14 17:37:34 +0600

    r

--- feb14/src/feb14/Feb14.java
+++ feb14/src/feb14/Feb14.java
@@ -1,5 +1,11 @@
 package feb14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Admin
@@ -7,7 +13,17 @@ package feb14;
 public class Feb14 {
 
     public static void main(String[] args) {
-        
+        PrintWriter pw = null;
+        try {
+            File file = new File("C:\\Users\\Admin\\Desktop\\javaPracticeFile.txt");
+            pw = new PrintWriter(file);
+            pw.write("Hello Sabbir \n");
+            pw.println("From  Mahbub");
+            pw.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Feb14.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
 }
