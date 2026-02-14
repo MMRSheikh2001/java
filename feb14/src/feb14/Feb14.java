cf332ffbdde6e7030f175b3d3beac4d4ce87b558
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-14 19:03:05 +0600

    vbvgfgh

--- feb14/src/feb14/Feb14.java
+++ feb14/src/feb14/Feb14.java
@@ -3,6 +3,7 @@ package feb14;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,22 +14,33 @@ import java.util.logging.Logger;
 public class Feb14 {
 
     public static void main(String[] args) {
-        PrintWriter pw = null;
         try {
+            PrintWriter pw = null;
             File file = new File("C:\\Users\\Admin\\Desktop\\javaPracticeFile.txt");
-            pw = new PrintWriter(file);
+            try {
+
+                pw = new PrintWriter(file);
 //            pw.write("Hello Sabbir \n");
 //            pw.println("From  Mahbub");
 
-            for (int i = 1; i <= 100; i++) {
+                for (int i = 1; i <= 1000; i++) {
 
-                pw.print(i + "   ");
-                if (i % 10 == 0) {
-                    pw.print("\n");
+                    pw.print(i + "\t");
+                    if (i % 10 == 0) {
+                        pw.print("\n");
+                    }
+
                 }
-
+                pw.close();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Feb14.class.getName()).log(Level.SEVERE, null, ex);
             }
-            pw.close();
+            
+            Scanner s = new Scanner(file);
+            while (s.hasNext()) {
+                System.out.println(s.nextLine());
+            }
+
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Feb14.class.getName()).log(Level.SEVERE, null, ex);
         }
