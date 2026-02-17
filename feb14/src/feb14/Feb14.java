c4f2303643a3d3c493655330fe24865f508aaa7b
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-17 19:18:05 +0600

    f

--- feb14/src/feb14/Feb14.java
+++ feb14/src/feb14/Feb14.java
@@ -14,36 +14,7 @@ import java.util.logging.Logger;
 public class Feb14 {
 
     public static void main(String[] args) {
-        try {
-            PrintWriter pw = null;
-            File file = new File("C:\\Users\\Admin\\Desktop\\javaPracticeFile.txt");
-            try {
-
-                pw = new PrintWriter(file);
-//            pw.write("Hello Sabbir \n");
-//            pw.println("From  Mahbub");
-
-                for (int i = 1; i <= 1000; i++) {
-
-                    pw.print(i + "\t");
-                    if (i % 10 == 0) {
-                        pw.print("\n");
-                    }
-
-                }
-                pw.close();
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(Feb14.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            Scanner s = new Scanner(file);
-            while (s.hasNext()) {
-                System.out.println(s.nextLine());
-            }
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Feb14.class.getName()).log(Level.SEVERE, null, ex);
-        }
+  
 
     }
 
