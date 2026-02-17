a10c153e6eff21087308f108badb12fa4d183d30
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-17 21:27:30 +0600

    errtr

---
+++ feb14/src/feb14/abstractPractice/Circle.java
@@ -0,0 +1,24 @@
+
+package feb14.abstractPractice;
+
+/**
+ *
+ * @author Mahbub
+ */
+public class Circle extends Geometry {
+
+    @Override
+    public double getArea() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double getPerimeter() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+    
+    
+}
---
+++ feb14/src/feb14/abstractPractice/Rectangle.java
@@ -0,0 +1,25 @@
+
+package feb14.abstractPractice;
+
+/**
+ *
+ * @author Mahbub
+ */
+public class Rectangle extends Geometry{
+
+    @Override
+    public double getArea() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double getPerimeter() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+    
+    
+    
+}
