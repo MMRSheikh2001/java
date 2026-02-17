838cb91c45d388c5c9110f399bdf37876fe031d5
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-17 21:58:09 +0600

    drdr

---
+++ feb14/src/feb14/abstractPractice/Chicken.java
@@ -0,0 +1,18 @@
+
+package feb14.abstractPractice;
+
+/**
+ *
+ * @author Mahbub
+ */
+public class Chicken implements Edible {
+
+    @Override
+    public String howToEat() {
+    return "Eat by Biting";
+    
+    }
+    
+    
+    
+}
