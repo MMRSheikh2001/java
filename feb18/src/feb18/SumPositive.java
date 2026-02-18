daa67344e4d0653e38824c9fc91e2a70c05c9526
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-18 17:27:15 +0600

    hygh

---
+++ feb18/src/feb18/SumPositive.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package feb18;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class SumPositive {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            sum = sum + n;
+            n = sc.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
