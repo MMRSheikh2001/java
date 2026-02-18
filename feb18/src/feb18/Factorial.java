188a06a0628b8b6e976c6b0667017383a33acb73
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-18 17:38:18 +0600

    gghh

---
+++ feb18/src/feb18/Factorial.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package feb18;
+
+/**
+ *
+ * @author Admin
+ */
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int multi =1;
+        for(int i=1; i<= n;i++){
+        multi = multi *i;
+        }
+        System.out.println(multi);
+    }
+}
