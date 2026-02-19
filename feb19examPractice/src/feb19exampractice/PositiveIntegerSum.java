e8e4ba8111deb020c3a929a6e5ad187d456ef719
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-19 16:29:39 +0600

    g

--- feb19examPractice/src/feb19exampractice/PositiveIntegerSum.java
+++ feb19examPractice/src/feb19exampractice/PositiveIntegerSum.java
@@ -1,4 +1,3 @@
-
 package feb19exampractice;
 
 import java.util.Scanner;
@@ -8,15 +7,16 @@ import java.util.Scanner;
  * @author Admin
  */
 public class PositiveIntegerSum {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Give only Positive Integers");
         int n = sc.nextInt();
-        int sum =0;
-        while(n>=0){
-        sum=n+sum;
-        n= sc.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            sum = n + sum;
+            n = sc.nextInt();
         }
-        System.out.println("The sum of positive integers are "+ sum);
+        System.out.println("The sum of positive integers are " + sum);
     }
 }
