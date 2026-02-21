1dcfd5cae912fb1c65623f05f09f563aba450d48
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-21 17:56:40 +0600

    fgfgd

--- feb19Recursion/src/feb19Recursion/MapsHash.java
+++ feb19Recursion/src/feb19Recursion/MapsHash.java
@@ -1,12 +1,21 @@
 
 package feb19Recursion;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Mahbub
  */
 public class MapsHash {
     public static void main(String[] args) {
+        HashMap<String, Integer> examScores= new HashMap<>();
+        examScores.put("Math", 76);
+        examScores.put("English", 80);
+        examScores.put("Bangla", 85);
+        examScores.put("Physics", 81);
+        examScores.put("Chemistry", 91);
         
+        System.out.println(examScores.toString());
     }
 }
--- feb19examPractice/src/feb19exampractice/MaxMin.java
+++ feb19examPractice/src/feb19exampractice/MaxMin.java
@@ -1,5 +1,8 @@
 package feb19exampractice;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Mahbub
@@ -7,6 +10,26 @@ package feb19exampractice;
 public class MaxMin {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numbers[] = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Give the " + (i + 1) + "th Number");
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
 
+        int min = numbers[0];
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+
+        }
+        System.out.println("The highest Number is "+ max);
+        System.out.println("The lowest Number is "+ min);
     }
 }
