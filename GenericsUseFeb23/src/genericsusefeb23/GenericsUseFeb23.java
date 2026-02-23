96ffb0a4f194e0b7f0170576869daf08a364c85d
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-23 14:26:37 +0600

    hhhhhhhhhhhhh

--- GenericsUseFeb23/src/genericsusefeb23/GenericsUseFeb23.java
+++ GenericsUseFeb23/src/genericsusefeb23/GenericsUseFeb23.java
@@ -1,5 +1,8 @@
 package genericsusefeb23;
 
+import genericsusefeb23.cllass.Student;
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
@@ -7,7 +10,18 @@ package genericsusefeb23;
 public class GenericsUseFeb23 {
 
     public static void main(String[] args) {
-        
+
+        ArrayList<Student> sList = new ArrayList<>();
+
+        sList.add(new Student(1, "Mahbub", "mahbub@gmail.com"));
+
+        Student s1 = new Student(2, "Sabbir", "sabbir@gmail.com");
+        sList.add(s1);
+        System.out.println(sList);
+        sList.forEach(item -> {
+            System.out.println(item);
+        });
+
     }
 
 }
