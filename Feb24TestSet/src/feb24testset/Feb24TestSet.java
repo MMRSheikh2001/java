501c9357145e9e4a89bddda469069db4702ec054
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-24 14:30:25 +0600

    ygh

---
+++ Feb24TestSet/src/feb24testset/Feb24TestSet.java
@@ -0,0 +1,24 @@
+
+package feb24testset;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Admin
+ */
+public class Feb24TestSet {
+
+    
+    public static void main(String[] args) {
+        HashSet<String> names= new HashSet<>();
+        names.add("Mahbub");
+        names.add("Sabbir");
+        names.add("Badrul");
+        names.add("Sajin");
+        names.add("Mahbub");
+        names.add("Rehana");
+        System.out.println(names);
+    }
+    
+}
---
+++ Feb24TestSet/src/feb24testset/MapHashTest.java
@@ -0,0 +1,21 @@
+package feb24testset;
+
+import java.util.HashMap;
+
+
+
+
+/**
+ *
+ * @author Admin
+ */
+public class MapHashTest {
+    public static void main(String[] args) {
+        HashMap<Integer,String> nameList= new HashMap<>();
+        nameList.put(1, "Mahbub");
+        nameList.put(2, "Tanvir");
+        nameList.put(3, "Jui");
+        nameList.put(4, "Sabbir");
+        System.out.println(nameList);
+    }
+}
