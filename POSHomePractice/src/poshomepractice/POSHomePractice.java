a5c52c883eacd87239c7dcb637cb47f57e3fbdf2
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-26 01:28:40 +0600

    ddf

--- POSHomePractice/src/poshomepractice/POSHomePractice.java
+++ POSHomePractice/src/poshomepractice/POSHomePractice.java
@@ -1,5 +1,7 @@
-
 package poshomepractice;
+
+import poshomepractice.dao.UserDao;
+import poshomepractice.model.User;
 
 /**
  *
@@ -11,6 +13,9 @@ public class POSHomePractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        User u = new User("Mahbub", "mahbub1234");
+        UserDao ud = new UserDao();
+        ud.saveUser(u);
         
     }
     
---
+++ POSHomePractice/src/poshomepractice/dao/UserDao.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poshomepractice.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import poshomepractice.model.User;
+import poshomepractice.util.DbUtil;
+
+/**
+ *
+ * @author Administrator
+ */
+public class UserDao {
+    DbUtil util = new DbUtil();
+    PreparedStatement ps;
+    ResultSet rs;
+    String sql = null;
+    
+    public void saveUser(User user){
+    sql = "insert into user(userName,password) values(?,?)";
+        try {
+            ps = util.getCon().prepareStatement(sql);
+            ps.setString(1, user.getUserName());
+            ps.setString(2, user.getPassword());
+            
+            ps.executeUpdate();
+            ps.close();
+            util.getCon().close();
+            System.out.println("Data Save (Home)");
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    
+}
