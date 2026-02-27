07f6a61e762a6179cd1d3ae65dfcb500232f66d8
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-28 05:46:29 +0600

    fhhf

--- POS/src/pos/dao/UserDao.java
+++ POS/src/pos/dao/UserDao.java
@@ -3,28 +3,32 @@ package pos.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import pos.model.User;
+import pos.service.DaoService;
 import pos.util.DbUtil;
 
 /**
  *
  * @author Admin
  */
-public class UserDao {
+public class UserDao implements DaoService<User>{
 
     DbUtil util = new DbUtil();
     PreparedStatement ps;
     ResultSet rs;
     String sql = null;
 
-    public void saveUser(User user) {
+    @Override
+    public void save(User e) {
+
         sql = "insert into user(userName ,password) values(?,?)";
         try {
             ps = util.getCon().prepareStatement(sql);
-            ps.setString(1, user.getUserName());
-            ps.setString(2, user.getPassword());
+            ps.setString(1, e.getUserName());
+            ps.setString(2, e.getPassword());
 
             ps.executeUpdate();
             ps.close();
@@ -35,7 +39,28 @@ public class UserDao {
         } catch (SQLException ex) {
             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
+
+    @Override
+    public List<User> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User findById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
 
 }
