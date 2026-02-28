70de53b2068a67476eaaff0bbd9bb23388da10be
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-02-28 16:18:32 +0600

    gfgf

--- binaryIO/src/binaryio/printWriterPractice.java
+++ binaryIO/src/binaryio/printWriterPractice.java
@@ -20,7 +20,7 @@ public class printWriterPractice {
         try {
             PrintWriter pw = new PrintWriter(file);
             for(int i =1;i<101;i++){
-            pw.println(i +" \t");
+            pw.write(i +" \t");
             }
             pw.println();
         } catch (FileNotFoundException ex) {
