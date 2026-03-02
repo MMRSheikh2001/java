b06559a12613349c339b6384d9bf0d6ae70436e7
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-03-02 13:41:36 +0600

    fbvgf

--- binaryIO/src/binaryio/printWriterPractice.java
+++ binaryIO/src/binaryio/printWriterPractice.java
@@ -15,18 +15,29 @@ import java.util.logging.Logger;
  * @author Admin
  */
 public class printWriterPractice {
-    public static void main(String[] args) {
-      File file= new File("C:\\Users\\Admin\\Desktop\\fileWriter.txt");
-        try {
-            PrintWriter pw = new PrintWriter(file);
-            for(int i =1;i<101;i++){
-            pw.write(i +" \t");
-            }
-            pw.println();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(printWriterPractice.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      
+   public static void main(String[] args) {
+//      File file= new File("C:\\Users\\Admin\\Desktop\\fileWriter.txt");
+//        try {
+//            PrintWriter pw = new PrintWriter(file);
+//            for(int i =1;i<101;i++){
+//            pw.write(i +" \t");
+//            }
+//            pw.println();
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(printWriterPractice.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    int i=10;
+    int j=0;
+       try {
+           double div= i/j;
+    
+       System.out.println(div);
+       } catch (Exception e) {
+           
+           System.out.println("Invalid");
+       }
+
+
     }
  //   PrintWriter output= new PrintWriter("C:\\Users\\Admin\\Desktop\\fileWriter.txt");
     
