8f8d67a5620837cd001322c82faa0abaa7b7c615
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-03-03 17:53:23 +0600

    ff

--- POS/src/pos/dao/SupplierDao.java
+++ POS/src/pos/dao/SupplierDao.java
@@ -2,7 +2,10 @@ package pos.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import pos.model.Supplier;
 import pos.service.DaoService;
 import pos.util.DbUtil;
@@ -12,35 +15,52 @@ import pos.util.DbUtil;
  * @author Administrator
  */
 public class SupplierDao implements DaoService<Supplier> {
+
     DbUtil db = new DbUtil();
     PreparedStatement ps;
     ResultSet rs;
     String sql;
-    
-    
+
     @Override
     public void save(Supplier e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        sql = "insert into supplier(name,cell,contactPersonName,contactPersonCell,address) values(?,?,?,?,?)";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, e.getName());
+            ps.setString(2, e.getCell());
+            ps.setString(3, e.getContactPersonName());
+            ps.setString(4, e.getContactPersonCell());
+            ps.setString(5, e.getAddress());
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+            System.out.println("Supplier Added");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SupplierDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Supplier not Added");
+        }
+
     }
-    
+
     @Override
     public List<Supplier> findAll() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public void update(Supplier e) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Supplier findById(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public void delete(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
