706b258f26a310013f61266f26d79d859237b25a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-03-05 15:57:39 +0600

    tyty

---
+++ EmployeeProject/src/employeeproject/dao/EmployeeDao.java
@@ -0,0 +1,65 @@
+package employeeproject.dao;
+
+import employeeproject.model.Employee;
+import employeeproject.util.DbUtil;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class EmployeeDao {
+
+    DbUtil db = new DbUtil();
+    String sql;
+    PreparedStatement ps;
+    ResultSet rs;
+
+    public void saveStudentData(Employee e) {
+        sql = "insert into employee (name,salary,email) values(?,?,?)";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, e.getName());
+            ps.setDouble(2, e.getSalary());
+            ps.setString(3, e.getEmail());
+
+            ps.executeUpdate();
+
+            ps.close();
+            db.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public List<Employee> findAllStudent() {
+        sql = "select * from employee";
+        List<Employee> eList = new ArrayList<>();
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Employee e = new Employee(rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getDouble("salary"),
+                        rs.getString("email"));
+                eList.add(e);
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return eList;
+
+    }
+
+}
