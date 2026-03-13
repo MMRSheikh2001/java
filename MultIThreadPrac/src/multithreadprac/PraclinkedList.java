638845bf953ca854aaf0e6727a9c7e4b0bbe663f
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-03-13 14:02:26 +0600

    fgf

---
+++ MultIThreadPrac/src/multithreadprac/PraclinkedList.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package multithreadprac;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PraclinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<String> c= new LinkedList<String>();
+        c.add("Red");
+        c.add("Blue");
+        System.out.println(c.get(1));
+    }
+}
