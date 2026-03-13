622f9d1086f7d2934a786b34cded299192b8656a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-03-14 00:43:17 +0600

    ttr

--- POS/src/pos/dao/ProductDao.java
+++ POS/src/pos/dao/ProductDao.java
@@ -4,10 +4,118 @@
  */
 package pos.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import pos.model.Product;
+import pos.service.DaoService;
+import pos.util.DbUtil;
+
 /**
  *
  * @author Administrator
  */
-public class ProductDao {
-    
+public class ProductDao implements DaoService<Product> {
+
+    DbUtil db = new DbUtil();
+    PreparedStatement ps;
+    ResultSet rs;
+    String sql;
+
+    @Override
+    public void save(Product e) {
+        sql = "insert into product (name,price,quantity,supplierId,categoryName) values(?,?,?,?,?)";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, e.getName());
+            ps.setDouble(2, e.getPrice());
+            ps.setDouble(3, e.getQuantity());
+            ps.setInt(4, e.getSupplierId());
+            ps.setString(5, e.getCategoryName());
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+            JOptionPane.showMessageDialog(null, "Product Saved");
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDao.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Product Not Saved");
+        }
+
+    }
+
+    @Override
+    public List<Product> findAll() {
+        List<Product> pList = new ArrayList<>();
+        sql = "select * from product";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Product p = new Product(rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getDouble("price"),
+                        rs.getDouble("quantity"),
+                        rs.getInt("supplierId"),
+                        rs.getString("categoryName"));
+                pList.add(p);
+            }
+            rs.close();
+            ps.close();
+            db.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return pList;
+    }
+
+    @Override
+    public void update(Product e) {
+        sql = "update product set name =?,price =?,quantity =?,supplierId=?,categoryName=? where id=?";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, e.getName());
+            ps.setDouble(2, e.getPrice());
+            ps.setDouble(3, e.getQuantity());
+            ps.setInt(4, e.getSupplierId());
+            ps.setString(5, e.getCategoryName());
+            ps.setInt(6, e.getId());
+            ps.executeUpdate();
+
+            ps.close();
+            db.getCon().close();
+            JOptionPane.showMessageDialog(null, "Product Updated");
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDao.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Product Not Updated");
+        }
+
+    }
+
+    @Override
+    public Product findById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(int id) {
+        sql = "delete from product where id=?";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+            JOptionPane.showMessageDialog(null, "Product Deleted");
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDao.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Product Not Deleted");
+        }
+    }
+
 }
--- POS/src/pos/model/Product.java
+++ POS/src/pos/model/Product.java
@@ -9,5 +9,85 @@ package pos.model;
  * @author Admin
  */
 public class Product {
-    
+
+    private int id;
+    private String name;
+    private double price;
+    private double quantity;
+    private int supplierId;
+    private String categoryName;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, double price, double quantity, int supplierId, String categoryName) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.supplierId = supplierId;
+        this.categoryName = categoryName;
+    }
+
+    public Product(String name, double price, double quantity, int supplierId, String categoryName) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.supplierId = supplierId;
+        this.categoryName = categoryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + ", supplierId=" + supplierId + ", categoryName=" + categoryName + '}';
+    }
+
 }
