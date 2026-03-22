5d1e970306457bf63bad2941cf79e15f28a0272a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-03-22 16:27:10 +0600

    ttr

---
+++ CodeWithHarry/lesson2/src/lesson2/PracticeSet.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PracticeSet {
+
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+        
+        //3
+        
+    }
+}
