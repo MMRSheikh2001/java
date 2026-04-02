eebafc550a47a3aefa4bf96594d851aa68b90103
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-02 16:06:20 +0600

    tygt

--- POSHomePractice/src/poshomepractice/util/DbUtil.java
+++ POSHomePractice/src/poshomepractice/util/DbUtil.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
 public class DbUtil {
 
     private Connection con = null;
-    private String url = "jdbc:mysql://localhost:3306/jeestore";
+    private String url = "jdbc:mysql://localhost:3306/homestore";
     private String user = "root";
     private String password = "1234";
     private String driver = "com.mysql.cj.jdbc.Driver";
