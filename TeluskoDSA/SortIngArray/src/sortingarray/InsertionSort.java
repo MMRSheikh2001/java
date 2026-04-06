1707a766c72ccdf020d1f21c09ae5418811e0e1c
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-06 22:13:56 +0600

    fdfv

---
+++ TeluskoDSA/SortIngArray/src/sortingarray/InsertionSort.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sortingarray;
+
+/**
+ *
+ * @author Administrator
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {23, 65, 76, 12, 32, 23, 45, 54, 34, 78, 9, 78, 90, 67, 45, 65, 56, 98};
+
+        
+        
+    }
+}
