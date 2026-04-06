69c6af47c7712e442548ccf5d1ac6bb738a7a1de
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-06 16:58:23 +0600

    ghjg

--- POSHomePractice/src/poshomepractice/dao/SalesDao.java
+++ POSHomePractice/src/poshomepractice/dao/SalesDao.java
@@ -4,19 +4,49 @@
  */
 package poshomepractice.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import poshomepractice.model.Sales;
 import poshomepractice.service.DaoService;
+import poshomepractice.util.DbUtil;
 
 /**
  *
  * @author Admin
  */
-public class SalesDao implements DaoService<Sales>{
+public class SalesDao implements DaoService<Sales> {
+
+    String sql;
+    PreparedStatement ps;
+    ResultSet rs;
+    DbUtil db = new DbUtil();
 
     @Override
     public void save(Sales e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        sql = "insert into sales (productName,unitPrice,quantity,totalPrice,discount,actualPrice,date) values(?,?,?,?,?,?,?)";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, e.getProductName());
+            ps.setDouble(2, e.getUnitPrice());
+            ps.setDouble(3, e.getQuantity());
+            ps.setDouble(4, e.getTotalPrice());
+            ps.setDouble(5, e.getDiscount());
+            ps.setDouble(6, e.getActualPrice());
+            ps.setDate(7, e.getSqlDate());
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+            JOptionPane.showMessageDialog(null, "Sales Saved");
+        } catch (SQLException ex) {
+            Logger.getLogger(SalesDao.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Sales Not Saved");
+        }
+
     }
 
     @Override
@@ -38,5 +68,5 @@ public class SalesDao implements DaoService<Sales>{
     public void delete(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
