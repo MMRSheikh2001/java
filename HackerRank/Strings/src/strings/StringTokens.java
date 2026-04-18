99a72066334f884db9a9b491042c4b088ea35656
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-18 13:47:49 +0600

    jyhhjy

---
+++ HackerRank/Strings/src/strings/StringTokens.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package strings;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class StringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+    }
+}
