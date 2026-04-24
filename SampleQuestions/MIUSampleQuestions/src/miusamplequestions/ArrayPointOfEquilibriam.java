ace97873ef93901545cf2f89d8b71e516a680b9a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-25 01:02:53 +0600

    dds

---
+++ SampleQuestions/MIUSampleQuestions/src/miusamplequestions/ArrayPointOfEquilibriam.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miusamplequestions;
+
+/**
+ * Consider an array A with n of positive integers. An integer idx is called a
+ * POE (point of equilibrium) of A, if A[0] + A[1] + … + A[idx – 1] is equal to
+ * A[idx + 1] + A[idx + 2] + … + A[n – 1]. Write a function to return POE of an
+ * array, if it exists and -1 otherwise. The signature of the function is: int
+ * f(int[] a)
+ *
+ */
+public class ArrayPointOfEquilibriam {
+    
+    public static void main(String[] args) {
+        int[] arr = {};
+        System.out.println(findArrayPointOfEquilibriam(arr));
+    }
+    
+    public static int findArrayPointOfEquilibriam(int[] arr) {
+        int eqil = -1;
+        
+        return eqil;
+    }
+    
+}
