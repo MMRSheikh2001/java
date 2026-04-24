57085da1ff8e99da3b27920e6ea0cd76ce57f484
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-25 00:54:20 +0600

    dfg

--- SampleQuestions/MIUSampleQuestions/src/miusamplequestions/CreatingArrayUsingCommonNumbersFrom2Arrays.java
+++ SampleQuestions/MIUSampleQuestions/src/miusamplequestions/CreatingArrayUsingCommonNumbersFrom2Arrays.java
@@ -4,6 +4,8 @@
  */
 package miusamplequestions;
 
+import java.util.Arrays;
+
 /**
  * Write a function to return an array containing all elements common to two
  * given arrays containing distinct positive integers. You should not use any
@@ -13,19 +15,38 @@ package miusamplequestions;
 public class CreatingArrayUsingCommonNumbersFrom2Arrays {
 
     public static void main(String[] args) {
-
+        int[] first = {1, 2, 3};
+        int[] second = {2, 3, 4};
+        int[] result = createCommonArrayItems(first, second);
+        System.out.println(Arrays.toString(result));
     }
 
     public static int[] createCommonArrayItems(int[] first, int[] second) {
         int count = 0;
+
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                if (first[i] == second[j]) {
+                    count++;
+                }
+
+            }
+
+        }
         int[] newArr = new int[count];
-        for(int i=0;i<first.length;i++){
-        for(int j=0;j<second.length;j++){
-        
-        
+        int index = 0;
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                if (first[i] == second[j]) {
+                    newArr[index] = first[i];
+                    index++;
+                    break;
+                }
+
+            }
+
         }
-        
-        }
+
         return newArr;
     }
 }
