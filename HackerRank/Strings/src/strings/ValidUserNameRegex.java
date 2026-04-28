8d44b1441520ab27882d1a4e9e60e5c6a46d4aca
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-28 17:42:53 +0600

    fgfhgg

---
+++ HackerRank/Strings/src/strings/ValidUserNameRegex.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package strings;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class ValidUserNameRegex {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
+
+class UsernameValidator {
+
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = null;
+}
