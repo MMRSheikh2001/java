6017fdf22ed5ca533fc9155c08da0e40a5811aae
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-03 16:42:09 +0600

    add

---
+++ JavaPrac3September/src/javaprac3september/JavaPrac3September.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaprac3september;
+
+import java.util.Scanner;
+
+
+public class JavaPrac3September {
+
+    
+    public static void main(String[] args) {
+        
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Give a number");
+//        int n = sc.nextInt();
+//        System.out.println("Given number is Even which is " + (n % 2 == 0));
+        Integer a = 127;
+        Integer b = 127;
+        System.out.println(a == b);
+
+    }
+
+}
