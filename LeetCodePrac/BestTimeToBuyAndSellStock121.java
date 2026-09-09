bcea4df92444b8e0f09e99dcf60ab8dfcdd3e19e
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-10 02:17:10 +0600

    add

--- LeetCodePrac/BestTimeToBuyAndSellStock121.java
+++ LeetCodePrac/BestTimeToBuyAndSellStock121.java
@@ -5,22 +5,46 @@ public class BestTimeToBuyAndSellStock121 {
     }
 }
 
+// class Solution {
+// public int maxProfit(int[] prices) {
+
+// int min = prices[0];
+// int profit = 0;
+// for (int i = 1; i < prices.length - 1; i++) {
+// if (prices[i] - min > profit) {
+// profit = prices[i] - min;
+// }
+// if (prices[i] < min) {
+// min = prices[i];
+// }
+// }
+
+// return profit;
+
+// }
+// }
+
 class Solution {
     public int maxProfit(int[] prices) {
-
-        int min = prices[0];
         int profit = 0;
+        int min = prices[0];
         for (int i = 1; i < prices.length - 1; i++) {
-            if (prices[i] - min > profit) {
-                profit = prices[i] - min;
-            }
+
             if (prices[i] < min) {
                 min = prices[i];
+            } else {
+                continue;
+            }
+
+            for (int j = i; j < prices.length; j++) {
+
+                if (prices[j] - min > profit) {
+                    profit = prices[j] - min;
+                }
             }
         }
 
         return profit;
-
     }
 }
 
