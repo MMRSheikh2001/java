9ea2e4696f87135d5199d265d1d168870af9b03e
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-10 02:30:44 +0600

    add

--- LeetCodePrac/BestTimeToBuyAndSellStock121.java
+++ LeetCodePrac/BestTimeToBuyAndSellStock121.java
@@ -24,42 +24,42 @@ public class BestTimeToBuyAndSellStock121 {
 // }
 // }
 
+// class Solution {
+// public int maxProfit(int[] prices) {
+// int profit = 0;
+// int min = prices[0];
+// for (int i = 1; i < prices.length - 1; i++) {
+
+// if (prices[i] < min) {
+// min = prices[i];
+// } else {
+// continue;
+// }
+
+// for (int j = i; j < prices.length; j++) {
+
+// if (prices[j] - min > profit) {
+// profit = prices[j] - min;
+// }
+// }
+// }
+
+// return profit;
+// }
+// }
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        int min = prices[0];
-        for (int i = 1; i < prices.length - 1; i++) {
-
-            if (prices[i] < min) {
-                min = prices[i];
-            } else {
-                continue;
-            }
-
+        int max = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i; j < prices.length; j++) {
 
-                if (prices[j] - min > profit) {
-                    profit = prices[j] - min;
+                if (prices[j] - prices[i] > max) {
+                    max = prices[j] - prices[i];
                 }
             }
         }
 
-        return profit;
+        return max;
     }
 }
-
-// class Solution {
-// public int maxProfit(int[] prices) {
-// int max = 0;
-// for (int i = 0; i < prices.length - 1; i++) {
-// for (int j = i; j < prices.length; j++) {
-
-// if (prices[j] - prices[i] > max) {
-// max = prices[j] - prices[i];
-// }
-// }
-// }
-
-// return max;
-// }
-// }
